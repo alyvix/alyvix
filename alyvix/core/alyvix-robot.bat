2e28290c48c213f3a43bdcbc6b40eb29fb6e307c
VioletAtom <VioletAtom@users.noreply.github.com>
2019-05-31 20:37:17 +0200

    removed garbage

--- alyvix/core/alyvix-robot.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call %~dp0..\python.exe %~dp0..\Lib\site-packages\alyvix\core\alyvix_robot.py %*
--- alyvix/ide/alyvix-designer.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call %~dp0..\python.exe %~dp0..\Lib\site-packages\alyvix\ide\alyvix_designer.py %*
