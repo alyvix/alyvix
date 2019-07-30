5c690dc6bf7221aa2989ffb653890a0dbe2dab52
Andrea Minetti <andrea.minetti@wavein.ch>
2019-07-30 07:55:01 +0200

    #167537056

--- alyvix-ui/src/app/ax-designer/box-options/t/t.component.ts
+++ alyvix-ui/src/app/ax-designer/box-options/t/t.component.ts
@@ -17,10 +17,12 @@ export class TComponent implements OnInit {
   constructor(private alyvixApi: AlyvixApiService, @Inject('GlobalRef') private global: GlobalRef, ) { }
 
   _node:TreeNode
+  loading:boolean
 
   @Input()
   set node(node: TreeNode) {
     this._node = node;
+    this.loading = true;
     this.onNodeChange();
   }
 
@@ -32,6 +34,7 @@ export class TComponent implements OnInit {
   regexpValidation = Validation.debouncedAsyncValidator<string>(v => {
     return this.alyvixApi.testScrapedText({ regexp: v, scraped_text: this.scraped }).pipe(map(res => {
       this.regExWarning = res.match == 'yellow'
+      this.loading = false;
       return res.match != 'red' ? null : { regexp: { invalidRegexp: v } }
     }))
 
@@ -49,6 +52,10 @@ export class TComponent implements OnInit {
 
   regExWarning:boolean = false
 
+  onRegexChange() {
+    this.node.box.features.T.regexp = this.regex.value;
+  }
+
   onNodeChange() {
     if (!this.node.box.features.T.type) {
       this.node.box.features.T.type = "detection";
@@ -58,15 +65,18 @@ export class TComponent implements OnInit {
         this.scraped = x.scraped_text;
         if (!this.node.box.features.T.regexp) {
           this.node.box.features.T.regexp = x.reg_exp
+          this.regex.setValue(x.reg_exp);
+        } else {
+          this.regex.setValue(this.node.box.features.T.regexp);
         }
       })
     }
 
     this.global.nativeGlobal().setTypeNode("T");
   }
-  
-  ngOnInit() { 
-    
+
+  ngOnInit() {
+
   }
 
 }
