b6c434a5e9e3b584ca9a06bc8988bae40245b946
VioletAtom <alan.pipitone@violetatom.com>
2019-11-02 18:31:26 +0100

    check, text, extract and maps args are now in yellow

--- alyvix/ide/server/views/__init__.py
+++ alyvix/ide/server/views/__init__.py
@@ -1134,7 +1134,10 @@ def test_txt_regexp():
         regexp = json_data["regexp"]
         scraped_text = json_data["scraped_text"]
 
-        args_in_string = re.findall("\\{[1-9]\d*\\}", regexp, re.IGNORECASE)
+        #args_in_string = re.findall("\\{[1-9]\d*\\}|\\{.*\\.extract\\}|\\{.*\\.text\\}|\\{.*\\.check\\}|\\{.*\\..*\\}",
+        #                            regexp, re.IGNORECASE)
+
+        args_in_string = re.findall("\\{[1-9]\d*\\}|\\{.*\\..*\\}", regexp, re.IGNORECASE)
 
         if len(args_in_string) > 0:
             ret_dict = {'match': 'yellow'}
